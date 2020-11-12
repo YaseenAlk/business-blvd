53514a33a0b96fac4b1e0f40493dcacf78eb752a
Nico Salinas <salinas.nsalinas@gmail.com>
2020-11-11 23:01:14 -0500

    Created new navbar components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   mounted() {
     axios.get('/heartbeat')
     .then(() => {
       console.log('Server connected.');
+    }).catch((err) => {
+      console.log(err.response.statusText);
     })
   }
 }
---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <Navbar />
+</template>
+
+<script>
+import Navbar from '../components/nav/Navbar.vue';
+export default {
+    name: 'Home',
+    components: {
+        Navbar,
+    },
+}
+</script>
+
+<style>
+
+</style>
