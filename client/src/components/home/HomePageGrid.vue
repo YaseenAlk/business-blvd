23bd55378e6f1b3f5dddd54ce0eaf7e2a3382d99
Nico Salinas <salinas.nsalinas@gmail.com>
2020-11-14 12:21:47 -0500

    Saved

---
+++ client/src/components/home/HomePageGrid.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="homepage-grid">
+        <b-card-group class="homepage-grid-item" v-for="card in cardData" v-bind:key="card.id" v-bind:id="card.id">
+            <b-card
+                class="mb-2"
+                v-bind:bg-variant="card.variant"
+            >
+                <b-button variant="primary">
+                {{card.title}}
+                </b-button>
+            </b-card>
+        </b-card-group>
+    </div>
+</template>
+
+<script>
+import { BCard, BButton } from 'bootstrap-vue';
+
+export default {
+    name: 'HomePageGrid',
+    components: {
+        BCard, BButton
+    },
+    data() {
+        return {
+            cardData: [
+                {id: 0, title: "Business Map", "button": "Go!", variant: "primary"},
+                {id: 1, title: "Business Search", "button": "", variant: "info"},
+                {id: 2, title: "Submit a Covid Safety Report", "button": "", variant: "warning"},
+                {id: 3, title: "TBD", "button": "", variant: "secondary"},
+                {id: 4, title: "Claim a Business", "button": "", variant: "danger"},
+                {id: 5, title: "My Account", "button": "", variant: "dark"},
+            ],
+        }
+    }
+}
+</script>
+
+<style>
+.homepage-grid {
+    display: grid;
+    margin: 32px;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 32px;
+}
+
+.homepage-grid-item {
+    width: 100%;
+    height: 100%;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
-    <p>hello world</p>
+  <div class="flex-row justify-content-center align-items-center">
+      <HomePageGrid />
+  </div>
 </template>
 
 <script>
+import HomePageGrid from '../components/home/HomePageGrid.vue';
+
 export default {
     name: 'Home',
+    components: {
+        HomePageGrid,
+    }
 }
 </script>
