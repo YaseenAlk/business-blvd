918727ad4a6d926f442dc78e4615ac97bb4c7e5d
Nico Salinas <salinas.nsalinas@gmail.com>
2020-11-14 01:46:21 -0500

    Added BootstrapVue use to main Vue app

--- client/src/main.js
+++ client/src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueCookie from 'vue-cookie';
+import { BootstrapVue } from 'bootstrap-vue';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueCookie);
+Vue.use(BootstrapVue);
 
 export const eventBus = new Vue();
 
