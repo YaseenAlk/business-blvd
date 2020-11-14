36d01b1fab9ad688624ba1ecf8d7069c591dbb36
Nico Salinas <salinas.nsalinas@gmail.com>
2020-11-14 17:09:45 -0500

    Updated client to use .env file

--- client/src/main.js
+++ client/src/main.js
@@ -8,11 +8,9 @@ import x5GMaps from 'x5-gmaps';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import { GOOGLE_API_KEY } from './KEYS.js';
-
 Vue.use(VueCookie);
 Vue.use(BootstrapVue);
-Vue.use(x5GMaps, GOOGLE_API_KEY);
+Vue.use(x5GMaps, { key: process.env.VUE_APP_GOOGLE_API_KEY });
 
 export const eventBus = new Vue();
 
