b0a94b1cc41e5c783ffeb7eb7f467a0e3f52fed3
Tyler Schoulte <tschoulte@gmail.com>
2020-11-15 01:25:12 -0600

    added BusinessRatings

--- server/src/models/Business.ts
+++ server/src/models/Business.ts
@@ -26,3 +26,27 @@ enum BusinessTags {
     DELIVERY,
     PICKUP
   }
+
+class BusinessRatings {
+    private _serviceRatingsMap: Map<string, number>;
+    private _safetyRatingsMap: Map<string, number>;
+
+    constructor(serviceRatingsMap: Map<string, number>, safetyRatingsMap: Map<string, number>) {
+        this._serviceRatingsMap = serviceRatingsMap;
+        this._safetyRatingsMap = safetyRatingsMap;
+    }
+
+    public updateServiceRating(customerId: string, rating: number) {
+        this._serviceRatingsMap.set(customerId, rating);
+    }
+    public getCustomerServiceRating(customerId: string): number | undefined {
+        return this._serviceRatingsMap.get(customerId);
+    }
+
+    public updateSafetyeRating(customerId: string, rating: number) {
+        this._safetyRatingsMap.set(customerId, rating);
+    }
+    public getSafetyServiceRating(customerId: string): number | undefined {
+        return this._safetyRatingsMap.get(customerId);
+    }
+}
