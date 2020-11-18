125534a1e6bd7b05b8b1f86d24683cf1878f8fed
YaseenAlk <alkhafaji.yaseen@gmail.com>
2020-11-18 15:42:49 -0500

    start planning rest contract for user model

---
+++ server/src/routers/users/UserRouter.ts
@@ -0,0 +1,51 @@
+/*
+Properties
+
+- Username
+- Password
+- Followed Businesses
+- Email
+
+Methods:
+
+- Change username?
+- Change Password?
+- Change email
+- Follow a Business
+- Unfollow a Business
+- Get followed businesses
+*/
+
+/*
+
+// Get user info
+GET /api/users/:id
+
+// create account
+POST /api/users
+{
+    email: "email@email.com",
+    username: "username",
+    password: "password"
+}
+
+// edit username or password (probably just email/password...)
+PUT /api/users/:id
+{
+    (optional) username: "username",
+    (optional) password: "password".
+    (optional) email: "email@email.com"
+}
+
+// delete account
+DELETE /api/users/:id
+
+// follow a business (uses session)
+POST /api/businesses/:id/follow
+
+// unfollow a business (uses session)
+DELETE /api/businesses/:id/follow
+
+// get followed businesses (uses session)
+GET /api/businesses/following
+*/
