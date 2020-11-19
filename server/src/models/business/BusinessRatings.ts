ef1203ce9d988050a41949a0a2415b2c789d826a
Tyler Schoulte <tschoulte@gmail.com>
2020-11-19 01:33:40 -0600

    added underscores

--- server/src/models/business/BusinessRatings.ts
+++ server/src/models/business/BusinessRatings.ts
@@ -1,27 +1,27 @@
 export default class BusinessRatings {
-  private serviceRatingsMap: Map<string, number> = new Map();
-  private safetyRatingsMap: Map<string, number> = new Map();
+  private _serviceRatingsMap: Map<string, number> = new Map();
+  private _safetyRatingsMap: Map<string, number> = new Map();
 
   //   constructor() {}
 
   static fromData(serviceRatingsMap: Map<string, number>, safetyRatingsMap: Map<string, number>): BusinessRatings {
     const ratings = new BusinessRatings();
-    ratings.serviceRatingsMap = new Map(serviceRatingsMap);
-    ratings.safetyRatingsMap = new Map(safetyRatingsMap);
+    ratings._serviceRatingsMap = new Map(serviceRatingsMap);
+    ratings._safetyRatingsMap = new Map(safetyRatingsMap);
     return ratings;
   }
 
   public updateServiceRating(customerId: string, rating: number): void {
-    this.serviceRatingsMap.set(customerId, rating);
+    this._serviceRatingsMap.set(customerId, rating);
   }
-  public getCustomerServiceRating(customerId: string): number | undefined {
-    return this.serviceRatingsMap.get(customerId);
+  public getServiceRating(customerId: string): number | undefined {
+    return this._serviceRatingsMap.get(customerId);
   }
 
   public updateSafetyRating(customerId: string, rating: number): void {
-    this.safetyRatingsMap.set(customerId, rating);
+    this._safetyRatingsMap.set(customerId, rating);
   }
-  public getSafetyServiceRating(customerId: string): number | undefined {
-    return this.safetyRatingsMap.get(customerId);
+  public getSafetyRating(customerId: string): number | undefined {
+    return this._safetyRatingsMap.get(customerId);
   }
 }
