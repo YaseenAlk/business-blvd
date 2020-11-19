b012de1a24eaba8b68bba3d9860911bb0a3b7ce3
YaseenAlk <alkhafaji.yaseen@gmail.com>
2020-11-19 02:24:02 -0500

    controller functions for auth

---
+++ server/src/controllers/UserController.ts
@@ -0,0 +1,29 @@
+import { User } from "../models/User";
+import { UserRepository } from "../repositories/UserRepository";
+
+class UserController {
+    signIn(session: any, username: string) {
+        const account = UserRepository.findOneByUsername(username);
+        session.userID = account.userID;
+        return {message: `Successfully logged in! Welcome ${username}.`, userId: account.userID, username:account.username};     
+    }
+
+    signOut(session: any) {
+        session.userID = undefined;
+        // we don't say their name on Goodbye for security reasons (e.g. if they were logged in on a public computer)
+        return {message: "Successfully logged out."};
+    }
+
+    getLoginStatus(session: any) {
+        let sessionId = session.userID;
+        let account = UserRepository.findOneByID(sessionId);
+        let response : any = {message: `You are ${sessionId ? '' : 'not'} signed in ${sessionId ? ` as ${account.username}` : '.'}`, signedIn: (sessionId ? true : false)};
+        if (sessionId) {
+            response.userId = account.userID;
+            response.username = account.username;
+        }
+        return response;
+    }
+}
+
+export = new UserController();
