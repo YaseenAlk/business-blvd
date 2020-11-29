a7efcf83b42220cd7b1c7cd4d7bdfa40269dbaa4
Johan Cervantes <jccamacho19992@gmail.com>
2020-11-29 14:22:24 -0800

    connected ratings

--- client/src/components/business/AtAGlance.vue
+++ client/src/components/business/AtAGlance.vue
@@ -3,26 +3,44 @@
         <h3 style='margin: 1em'>At a Glance</h3>
         <b-list-group>
             <b-list-group-item class='d-flex justify-content-between align-items-center'>
-                <b-icon icon='star-fill' scale="2" variant="danger"></b-icon>{{ this.business.rating}} Rating <br/>
+                Rating: {{ serviceRating }}/5  <b-icon icon='star-fill' scale="2" variant="danger"></b-icon>
             </b-list-group-item>
             <b-list-group-item class='d-flex justify-content-between align-items-center'>
-                <b-icon icon='heart-fill' scale="2" variant="danger"></b-icon>{{ this.business.covidScore}} Covid Safety Score <br/>
+                 Covid Safety Score: {{ safetyRating }}/5 <b-icon icon='heart-fill' scale="2" variant="danger"></b-icon>
             </b-list-group-item>
             <b-list-group-item class='d-flex justify-content-between align-items-center'>
-                <b-icon icon='shield-plus' scale="2" variant="danger"></b-icon> Hand sanitizer available <br/>
+                 Hand sanitizer available <b-icon icon='shield-plus' scale="2" variant="danger"></b-icon>
             </b-list-group-item>
             <b-list-group-item class='d-flex justify-content-between align-items-center'>
-                <b-icon icon='people' scale="2" variant="danger"></b-icon> Capacity limit: 5 <br/>
+                 Capacity limit: 5 <b-icon icon='people' scale="2" variant="danger"></b-icon>
             </b-list-group-item>
         </b-list-group>
     </div>
 </template>
 
 <script>
+
+const getAverage = (ratings) => {
+  let net = 0;
+  const vals = Object.values(ratings);
+  vals.forEach((val) => {
+    net += val
+  })
+  return net/vals.length;
+}
+
 export default {
     name: 'AtAGlance',
     props: {
       business: Object,
+    },
+    data() {
+      let safetyRating = getAverage(this.business.ratings._safetyRatingsMap);
+      let serviceRating = getAverage(this.business.ratings._serviceRatingsMap);
+      return {
+        safetyRating,
+        serviceRating
+      }
     }
 }
 </script>
