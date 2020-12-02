188d4b594fc5ae2e43c659983f59e33ca77d2979
Johan Cervantes <jccamacho19992@gmail.com>
2020-12-02 01:13:52 -0800

    position changes

--- server/src/repositories/business/PositionRepository.ts
+++ server/src/repositories/business/PositionRepository.ts
@@ -5,7 +5,7 @@ class PositionRepository {
   generateExamples(): Promise<Position> {
     const [b1, b2] = BusinessRepository.getExampleBusinessIDs();
     const p1 = new Position(b1, '123 Seasame Street', 42.362541, -71.09845);
-    const p2 = new Position(b2, '123 Seasame Street', 42.362541, -71.09845);
+    const p2 = new Position(b2, '124 Seasame Street', 42.362545, -71.09855);
     return p1.save().then(() => {
       return p2.save();
     });
