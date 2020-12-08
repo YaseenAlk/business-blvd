98cdda5bce92c8c20dac06b8f654cefbb6445887
YaseenAlk <alkhafaji.yaseen@gmail.com>
2020-12-08 00:47:31 -0500

    add timestamp column

--- server/src/models/Update.ts
+++ server/src/models/Update.ts
@@ -20,15 +20,16 @@ export default class Update extends BaseEntity {
   seenBy: string[];
 
   // this could also be stored when the update is created, which would allow the repository to send back updates in order of timestamp
-  // @Column('timestamp')
-  // dateCreated: Date
+  @Column('timestamp')
+  dateCreated: Date;
 
-  constructor(authorId?: string, businessId?: string, content?: string) {
+  constructor(authorId?: string, businessId?: string, content?: string, creationTime?: Date) {
     super();
     this.id = uuidv4();
     this.authorId = authorId || uuidv4();
     this.businessId = businessId || uuidv4();
     this.content = content || '';
     this.seenBy = [];
+    this.dateCreated = creationTime || new Date();
   }
 }
