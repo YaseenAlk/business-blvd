5c34a7f5b869635e866de81015689f17710e1293
Johan Cervantes <45345485+johancc@users.noreply.github.com>
2020-12-08 19:04:32 -0500

    Updates ratings (#71)
    * Add a lil bit of ~spice~
    
    * Optimized form logic
    
    * Added textarea
    
    * Fixed validation bug

--- client/src/views/Report.vue
+++ client/src/views/Report.vue
@@ -4,17 +4,22 @@
       <h3>Rate a Business</h3>
       <div>Submit a Business review based on how their service was, how they complied with COVID safety polices, and what you general review of the business is.</div>
       <b-form id="review-form" @submit.prevent="onSubmit" class="report-page form-content">
-        <b-form-group label="Select a business: " label-for="businessSelect" label-align="left" label-cols-sm="4">
-          <b-form-select id="businessSelect" v-model="form.selected" :options="options" required />
-        </b-form-group>
-        <b-form-group label="COVID Safety Rating: " label-for="safetyRating" label-align="left" label-cols-sm="4">
-            <b-form-input id="safetyRating" type="number" :min="0" :max="5" v-model="form.safetyRating" size="sm" required/>
-        </b-form-group>
-        <b-form-group label="Service Rating: " label-for="service" label-align="left" label-cols-sm="4">
-            <b-form-input id="serviceRating" type="number" :min="0" :max="5" v-model="form.serviceRating" size="sm" required/>
+        <b-form-group  label="Select a business: " label-for="businessSelect" label-align="left" label-cols-sm="4">
+          <b-form-select placeholder="Please select a business to rate" id="businessSelect" v-model="form.selected" :options="options" required />
         </b-form-group>
         <b-form-group label="Review: " label-for="review" label-align="left" label-cols-sm="4">
-            <b-form-input id="review" type="text" v-model="form.review" size="sm" required/>
+            <b-form-textarea id="review" type="text" v-model="form.review" size="md" rows="3" max-rows="6" placeholder="Share your experience with others!" required/>
+        </b-form-group>
+        <b-form-group label="Safety Rating" label-align="left" label-cols-sm="4">
+         <b-form-rating
+          icon-empty="heart"
+          icon-half="heart-half"
+          icon-full="heart-fill"
+          v-model="form.safetyRating"
+          variant="danger"></b-form-rating>
+        </b-form-group>
+        <b-form-group label="Service Rating" label-align="left" label-cols-sm="4">
+          <b-form-rating v-model="form.serviceRating" variant="primary"></b-form-rating>
         </b-form-group>
         <b-button type="submit" variant="success" style="margin-bottom: 12px">
           <div class="d-flex align-items-center">
@@ -34,42 +39,36 @@ import { BForm, BFormGroup, BAlert } from 'bootstrap-vue';
 
 export default {
   name: 'Report',
+  components: {
+    BForm, 
+    BFormGroup, 
+    BAlert
+  },
   beforeCreate:  function() {
     // We need to load all the data so the 
     // user can choose which business to review 
     // using a dropdown.
     // This is so inefficient.
     axios.get('/api/business/all')
-      .then((resp) => {
-        return resp.data;
-      })
+      .then((resp) => resp.data)
       .then((businesses) => {
-        const options = [];
-        businesses.map(({ name, businessId }) =>  {
-          options.push({name, businessId});
+        businesses.map((business) => {
+          this.options.push({ text: business.name, value: business.businessId})
         })
-        return options;
       })
-      .then((options) => {
-        this.options = options.map(({name}) => name);
-        this.businesses = options;
-      })
-      .catch((err) => {
-        console.error(err )
-      });
+      .catch(console.err);
   },
   data() {
     return { 
-      options: undefined,
+      options: [{value: null, text: 'Please select a business'}],
       form: {
-        selected: undefined,
+        selected: null,
         safetyRating: undefined,
         serviceRating: undefined,
         review: undefined,
       },
       error: undefined,
       success: undefined,
-      businesses: undefined,
       loading: false,
     };
   },
@@ -80,23 +79,16 @@ export default {
     onSubmit: function(){
         this.clearAlerts();
         this.loading = true;
-        const selectedBusiness = this.businesses
-          .filter((o) => o.name === this.form.selected)[0];
-        console.log(this.form)
-        axios.put(`/api/business/${selectedBusiness.businessId}/ratings/`, this.form)
-        .then(() => {
-          this.success = "Submitted rating successfully!";
-          this.loading = false;
-        })
-        .catch((err) => {
-          this.error = err.response.data.message || err;
-          this.loading = false;
-        });
+        axios.put(`/api/business/${this.form.selected}/ratings/`, this.form)
+          .then(() => {
+            this.success = "Submitted rating successfully!";
+          })
+          .catch((err) => {
+            this.error = err.response.data.message || err;
+          })
+          .finally(() => this.loading = false);
     }
   },
-  components: {
-    BForm, BFormGroup, BAlert
-  }
 }
 </script>
 <style scoped>
--- server/src/Constants.ts
+++ server/src/Constants.ts
@@ -1,4 +1,4 @@
 export const MAX_SAFETY_RATING = 5;
-export const MAX_SERVICE_RATING = 0;
-export const MIN_SAFETY_RATING = 5;
+export const MAX_SERVICE_RATING = 5;
+export const MIN_SAFETY_RATING = 0;
 export const MIN_SERVICE_RATING = 0;
